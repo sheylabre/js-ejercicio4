71d2879f665314d858efc2921619ccdb3f8ed010
sheylabre <sheylabrena@hotmail.com>
2016-08-25 15:27:59 -0500

    ejercicio4

---
+++ app.js
@@ -0,0 +1,7 @@
+var tiempo = parseInt(prompt("tiempo"));
+var costoMinuto = parseInt(prompt("Costo por minuto"));
+
+var resul = tiempo * costoMinuto;
+document.write("Costo de la llamada telefónica:" + resul) ;
+
+
